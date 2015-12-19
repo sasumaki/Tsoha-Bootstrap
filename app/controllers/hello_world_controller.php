e6cdc500f1e8a67a43ab78fb73d46fd5a9f894d4
Sasu Mäkinen <sasumaki@dhcp-asv-121.helsinki.fi>
2015-12-19 17:16:52 +0200

    Perus HTML suunnitelmat

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,31 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function lista() {
+        View::make('suunnitelmat/lista.html');
+    }
+
+    public static function show() {
+        View::make('suunnitelmat/show.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+    public static function modify() {
+        View::make('suunnitelmat/modify.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,22 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+$routes->get('/lista', function() {
+    HelloWorldController::lista();
+});
+$routes->get('/show', function() {
+    HelloWorldController::show();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+$routes->get('/modify', function() {
+    HelloWorldController::modify();
+});
